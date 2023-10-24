6d72a0a46a98c06e3342fc222c9dc1302677b265
Dmitriy Trifonov <dmitriy.trifonov@vk.team>
2023-10-24 19:35:32 +0300

    add bloom filter structure

--- bloom_filter/bloom_filter.py
+++ bloom_filter/bloom_filter.py
@@ -2,31 +2,31 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.bit_array = 0
+        self.array_of_bites = 0
 
     def hash1(self, str1):
-        res = 0
+        result = 0
         for c in str1:
             code = ord(c)
-            res = (res * 17 + code) % self.filter_len
-        return pow(2, res)
+            result = (result * 17 + code) % self.filter_len
+        return pow(2, result)
 
     def hash2(self, str1):
-        res = 0
+        result = 0
         for c in str1:
             code = ord(c)
-            res = (res * 223 + code) % self.filter_len
-        return pow(2, res)
+            result = (result * 223 + code) % self.filter_len
+        return pow(2, result)
 
     def add(self, str1):
         hs1 = self.hash1(str1)
         hs2 = self.hash2(str1)
-        self.bit_array = self.bit_array | hs1
-        self.bit_array = self.bit_array | hs2
+        self.array_of_bites = self.array_of_bites | hs1
+        self.array_of_bites = self.array_of_bites | hs2
 
     def is_value(self, str1):
         hs1 = self.hash1(str1)
         hs2 = self.hash2(str1)
-        check_first = self.bit_array & hs1 != 0
-        check_second = self.bit_array & hs2 != 0
-        return check_first and check_second
+        check_first_elem = self.array_of_bites & hs1 != 0
+        check_second_elem = self.array_of_bites & hs2 != 0
+        return check_first_elem and check_second_elem
